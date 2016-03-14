133452739dcc86892a85b976f3192c85822e4012
Yves Zieger <yves.zieger@googlemail.com>
2016-03-14 13:59:44 +0100

    fix for Puppet 4

---
+++ manifests/mac_defaults.pp
@@ -0,0 +1,32 @@
+define macdefaults::mac_defaults (
+    $domain, 
+    $key, 
+    $value = false, 
+    $type = "string", 
+    $action = "write"
+) {
+    case $operatingsystem {
+    'Darwin':{
+           case $action {
+               "write": {
+                   exec {"defaults write $domain $key -$type '$value'":
+                       path => "/bin:/usr/bin",
+                       unless => $type ? {
+                            'bool' => $value ? {
+                                'TRUE' => "defaults read $domain $key | grep -qx 1",
+                                'FALSE' => "defaults read $domain $key | grep -qx 0"
+                               },
+                            default => "defaults read $domain $key | grep -qx $value | sed -e 's/ (.*)/\1/'"
+                            }
+                       }
+                   }
+               "delete": {
+                   exec {"defaults delete $domain $key":
+                       path   => "/bin:/usr/bin",
+                       onlyif => "defaults read $domain | grep -q '$key'"
+                   }
+                }
+            }
+        }
+    }
+}
